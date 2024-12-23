b000c506a7a5b178e5e65a362aa7a891744203f0
yigitcankaragoz <56552273+yigitcankaragoz@users.noreply.github.com>
2024-12-23 22:21:36 +0300

    Create Day - 1.py

---
+++ Day - 1.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator")
+
+question1 = input("What's the name of the city you grew up in ?\n")
+
+
+question2 = input("What's your pet's name ?\n")
+
+
+print("Your band name could be"+" "+ question1 +" "+question2)
